6f857a7d30721463d3544475dcd9912e063c4ddf
Jempe <info@jempe.org>
2022-01-08 18:24:56 -0500

    API

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/qor/admin"
+	"github.com/qor/qor"
 
 	"github.com/jempe/shopping_list/models/lists"
 )
@@ -30,9 +31,37 @@ func main() {
 	Admin.AddResource(&lists.List{})
 	Admin.AddResource(&lists.Item{})
 
+	API := admin.New(&qor.Config{DB: gormDB})
+
+	API.AddResource(&lists.List{})
+	item := API.AddResource(&lists.Item{})
+
+	item.Action(&admin.Action{
+		Name: "check",
+		Handler: func(actionArgument *admin.ActionArgument) error {
+			for _, record := range actionArgument.FindSelectedRecords() {
+				actionArgument.Context.DB.Model(record.(*lists.Item)).Update("Checked", true)
+			}
+
+			return nil
+		},
+	})
+
+	item.Action(&admin.Action{
+		Name: "uncheck",
+		Handler: func(actionArgument *admin.ActionArgument) error {
+			for _, record := range actionArgument.FindSelectedRecords() {
+				actionArgument.Context.DB.Model(record.(*lists.Item)).Update("Checked", false)
+			}
+
+			return nil
+		},
+	})
+
 	mux := http.NewServeMux()
 
 	Admin.MountTo("/admin", mux)
+	API.MountTo("/api", mux)
 
 	port := "3000"
 
